ae891651bb4c3f3d16983da994847d9ed74bdb6f
taocore <taocore@44cb530b-7600-76e4-cbcd-383d488aed27>
2012-02-05 02:34:01 +0000

    js

---
+++ taobao-seo/WebContent/js/jquery-ui-timepicker-zh-CN.js
@@ -0,0 +1,16 @@
+/* Chinese translation for the jQuery Timepicker Addon */
+(function($) {
+	$.timepicker.regional['zh-CN'] = {
+		timeOnlyTitle: '选择时间',
+		timeText: '时间',
+		hourText: '小时',
+		minuteText: '分钟',
+		secondText: '秒',
+		timezoneText: '时区',
+		currentText: '现在',
+		closeText: '确定',
+		timeFormat: 'hh:mm',
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['zh-CN']);
+})(jQuery);
---
+++ taobao-seo/WebContent/js/jquery.mytable.js
@@ -0,0 +1,43 @@
+﻿(function($) {
+
+    $.fn.mytable = function(options) {
+        var opts = $.extend({}, $.fn.mytable.defaults, options);
+        this.each(function() {
+            apply($(this), options);
+        });
+    };
+
+    function apply(table, options) {
+    	$("tbody input.selector", table).change(function(){
+    		var selected = $(this).attr("checked");
+    		var row = $(this).closest("tr");
+    		//alert(row.html());
+    		if (selected)
+    		{
+    			row.addClass("row-selected");
+    		}
+    		else
+    		{
+    			row.removeClass("row-selected");
+    			$("thead input.selector", table).attr("checked", false);
+    		}
+    		options.selectionChanged(row);
+    	});
+
+    	$("thead input.selector", table).change(function(){
+    		if($(this).attr("checked"))
+    		{
+    			$("tbody input.selector", table).attr("checked", 'checked');
+    		}
+    		else
+    		{
+    			$("tbody input.selector", table).removeAttr("checked");
+    		}
+    		$("tbody input.selector", table).trigger('change');
+    	});
+    }
+    
+    $.fn.mytable.defaults = {
+			
+    };
+})(jQuery);
